5aab97d97ac5cc554e8e472b59defe3d5ab07d3e
Aamir P <79377502+Best-forever-003@users.noreply.github.com>
2021-09-13 15:01:10 +0530

    Add files via upload

---
+++ generate even numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=100;i++)
+    {
+        i = i+1;
+        printf("%d \n \n", i);
+    }
+    return 0;
+}
---
+++ generate odd numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=0;i<100;i++)
+    {
+        i = i+1;
+        printf("%d \n \n", i);
+    }
+    return 0;
+}
