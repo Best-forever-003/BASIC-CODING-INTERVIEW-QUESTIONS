bb57087a94ab4635f8ab1e0130ec65630b35999e
Aamir P <79377502+Best-forever-003@users.noreply.github.com>
2021-09-13 14:55:53 +0530

    Add files via upload

---
+++ odd or even.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the input: ");
+    scanf("%d",&n);
+    if(n%2==0)
+    printf("even number");
+    else
+    printf("odd number");
+    return 0;
+}
