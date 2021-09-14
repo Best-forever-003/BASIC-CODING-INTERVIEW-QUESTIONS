4e36faade73b56ab869610dd13388656265ce8b8
Aamir P <79377502+Best-forever-003@users.noreply.github.com>
2021-09-15 00:05:36 +0530

    Add files via upload

---
+++ string palindrome.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[1000];
+    int i,c=0, n;
+    
+    printf("Enter the input: ");
+    gets(s);
+    n=strlen(s);
+    
+    for(i=0;i<n/2;i++)
+    {
+        if(s[i]==s[n-i-1])
+        c++;
+    }
+    
+    if(c==i)
+    printf("Palindrome");
+    else
+    printf("Not a Palindrome");
+    
+    return 0;
+}
