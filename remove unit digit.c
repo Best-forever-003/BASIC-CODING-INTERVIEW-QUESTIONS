dc6b53d3e61d58a7e96013e78a91872652fe5aea
Aamir P <79377502+Best-forever-003@users.noreply.github.com>
2021-09-16 01:10:39 +0530

    Add files via upload

---
+++ remove unit digit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    
+    printf("Enter the input: ");
+    
+    scanf("%d", &i);
+    
+    i = i/10;
+    
+    printf("%d", i);
+    
+    return 0;
+}
